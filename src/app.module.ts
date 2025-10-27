bd5fb6854a6ce741f58a06f37cf065a9ed955f44
zakaria <himranezakaria2@gmail.com>
2025-10-27 19:12:59 +0100

    Connect with the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 @Module({
-  imports: [TasksModule, UsersModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: process.env.DB_PASSWORD,
+    database: 'taskDB',
+    autoLoadEntities: true,
+    synchronize: true
+  }), TasksModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
